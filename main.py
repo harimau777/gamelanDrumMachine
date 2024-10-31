e2dc870a532ee6d044f8ad6b4ba16fc295b514c6
harimau777 <foo@bar.com>
2024-10-31 10:00:37 -0500

    Draw initial UI boxes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from draw_ui import draw_ui
 from config import WIDTH, HEIGHT, FPS, BLACK, WHITE, GRAY, LABEL_FONT
 from setup import screen, clock
 
@@ -7,6 +8,7 @@ run = True
 while run:
     clock.tick(FPS)
     screen.fill(BLACK)
+    draw_ui()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
